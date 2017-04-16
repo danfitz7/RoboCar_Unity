703f886f36fd63f67f71fca912dd9d82c333cbd6
Daniel Fitzgerald <djfitz@mit.edu>
2017-04-16 18:58:30 -0400

    pretty much there

--- RoboCar1/Assets/Scripts/RobotFollowDriver.cs
+++ RoboCar1/Assets/Scripts/RobotFollowDriver.cs
@@ -14,15 +14,25 @@ using Consistent_Overhead_Byte_Stuffing;
 
 public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
 {
-	const float FOLLOWING_DISTANCE_M = 1.5f; // How far behind to follow the target, in meters
+	const float FOLLOWING_DISTANCE_M = 1.25f; // How far behind to follow the target, in meters
 
-    //const string DEFAULT_ARDUINO_PORT_NAME = "\\\\.\\COM7";//"COM6";
+    
+	#if UNITY_EDITOR_WINDOWS
+		const string DEFAULT_ARDUINO_PORT_NAME = "\\\\.\\COM7";//"COM6";
+	#elif UNITY_EDITOR_OSX
+		const string DEFAULT_ARDUINO_PORT_NAME = "/dev/tty.usbmodem2729671";//"/dev/tty.usbmodem1421";
+	#elif UNITY_ANDROID
+		const string DEFAULT_ARDUINO_PORT_NAME = "/dev/bus/usb/001/002";
+	#else
+		const string DEFAULT_ARDUINO_PORT_NAME = "UNDEFINED DEFAULT PORT FOR THIS PLATFORM";
+	#endif
+
     const int SERIAL_BAUD = 9600;
     const string ARDUINO_ACK = "ACK";
-    //const string DRIVE_ARDUINO_ADDRESS = "255";
+    const string DRIVE_ARDUINO_ADDRESS = "255";
 
  //   static readonly string DRIVE_COMMAND = "DRIVE";
-    static readonly int DRIVE_MESSAGE_DATA_BYTES = (2 * 3); // One byte delimiter + 2 numbers, 2 bytes each
+    static readonly int DRIVE_MESSAGE_DATA_BYTES = (2 * 2); // One byte delimiter + 2 numbers, 2 bytes each
     const byte COBS_DELIMITER = 0;
     byte[] driveMessageData;
     byte[] COBSEncodedMessage = new byte[DRIVE_MESSAGE_DATA_BYTES + DRIVE_MESSAGE_DATA_BYTES/254 +1];
@@ -30,16 +40,19 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
     const int DEFAULT_ARDIUNO_PORT_WRITE_TIMEOUT_MS = 16;   // 16.67ms between frames at 60fps
    
     System.IO.Ports.SerialPort arduinoSerialPort;
-	string arduinoPortName = "";//DEFAULT_ARDUINO_PORT_NAME;
+	string arduinoPortName = DEFAULT_ARDUINO_PORT_NAME;
 
+	// For tracking calculations
 	private TrackableBehaviour mTrackableBehaviour;
 	bool tracking = false;
-
 	GameObject ARCamera;
 
     // These are the actual values we are trying to convey to the arduino
     float distanceError_m = 0.0f;
     float angleError_deg = 0.0f;
+
+	const float COMMUNICATION_INT16_MAX = 32767.0f;
+	const float twoPi = (float)Math.PI * 2.0f; 
 
     // Shamelessly copied from https://forum.unity3d.com/threads/serial-communication-between-unity-5-and-arduino.331005/
     List<String> GetPortNames(){
@@ -78,11 +91,13 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
         return serial_ports;
     }
 
-    //void sendAck(){
-    //    print("Sending ACK...");
-    //    arduinoSerialPort.Write(ARDUINO_ACK);
-    //    arduinoSerialPort.BaseStream.Flush();
-    //}
+    void sendAck(){
+        print("Sending ACK...");
+		if (arduinoSerialPort != null && arduinoSerialPort.IsOpen) {
+			arduinoSerialPort.Write (ARDUINO_ACK);
+			arduinoSerialPort.BaseStream.Flush ();
+		}
+    }
 
     // Open Serial Port connection to Arduino
     public void OpenArduinoSerialPortConnection(){
@@ -108,16 +123,12 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
     }
 
     // Close any open serial port connection before terminating
-    void OnApplicationQuit(){
+	void OnApplicationQuit(){
 		if (arduinoSerialPort != null && arduinoSerialPort.IsOpen) {
 			arduinoSerialPort.Close ();
 			Debug.Log ("Arduino Port closed!");
 		}
     }
-
-//    void arduinoSerialReadCallback(String s){
-//       print("Received: " + s);
-//    }
 
 	/*
     // Arduino serial read asynch callback (doesn't clog up main loop)
@@ -125,80 +136,80 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
         DateTime initialTime = DateTime.Now;
         DateTime nowTime;
         TimeSpan diff = default(TimeSpan);
-
         string dataString = null;
-
-        do
-        {
-            try
-            {
+        do{
+            try{
                 dataString = arduinoSerialPort.ReadLine();
-            }
-            catch (TimeoutException)
-            {
+            }catch (TimeoutException){
                 dataString = null;
             }
 
-            if (dataString != null)
-            {
+            if (dataString != null){
                 callback(dataString);
                 yield return null;
-            }
-            else
+			}else{
                 yield return new WaitForSeconds(0.05f);
-
+			}
             nowTime = DateTime.Now;
             diff = nowTime - initialTime;
 
-        } while (diff.Milliseconds < timeout);
-
-        if (fail != null)
-            fail();
+        }
+		while (diff.Milliseconds < timeout);
+		if (fail != null) {
+			fail ();
+		}
         yield return null;
     }
 	*/
 
-    void sendDriveCommand(){
+	void sendDriveCommand(){
+		if (this.enabled) {
 
-        //print("Sending drive command");
-        
-		// Unity is in meters, so multiply by 10,000 to get 100um as base unit
-        Int16 distError = (Int16)(distanceError_m * 10000.0f + 0.5f);
-        
-		// Convert angle and ensure it is in [-180, 180] degrees
-		const float twoPi = (float)Math.PI * 2.0f; 
-		float rotationErrorRadians = angleError_deg * Mathf.Deg2Rad;
-        while (rotationErrorRadians <= -Math.PI){    // If we're less than -180 degrees, add a rotation so we're in -180<r<180
-            rotationErrorRadians += twoPi;
-        }
-        while (rotationErrorRadians >= Math.PI){    // if we're more than +180 degrees, subtract a rotation so we're in -180<r<180
-            rotationErrorRadians -= twoPi;
-        }
-        Int16 rotationErrorInt = (Int16)((rotationErrorRadians * 32767) / Math.PI);
+			//print("Sending drive command");
+	        
+			// Unity is in meters, so multiply by 10,000 to get 1mm as base unit
+			float distErrorf = distanceError_m * 1000.0f;
+			Int16 distError = (Int16)Math.Round(distErrorf);
+			if (distErrorf > COMMUNICATION_INT16_MAX) {
+				distError = (Int16)(COMMUNICATION_INT16_MAX);
+			} else if (distError < -COMMUNICATION_INT16_MAX) {
+				distError = (Int16)(-COMMUNICATION_INT16_MAX);
+			}
 
-        print("Drive<" + distError/10.0 + "mm\t" + ((((float)rotationErrorInt)*180.0f)/32767.0f).ToString() + "deg>");
+			// Convert angle and ensure it is in [-180, 180] degrees
+			float rotationErrorRadians = angleError_deg * Mathf.Deg2Rad;
+			while (rotationErrorRadians <= -Math.PI) {    // If we're less than -180 degrees, add a rotation so we're in -180<r<180
+				rotationErrorRadians += twoPi;
+			}
+			while (rotationErrorRadians >= Math.PI) {    // if we're more than +180 degrees, subtract a rotation so we're in -180<r<180
+				rotationErrorRadians -= twoPi;
+			}
+			Int16 rotationErrorInt = (Int16)(Math.Round((rotationErrorRadians * COMMUNICATION_INT16_MAX) / Math.PI));
+
+//			print ("Drive<" + distError + "mm\t" + ((((float)rotationErrorInt) * 180.0f) / 32767.0f).ToString () + "deg>");
 
 
-        // Byte stuffing, yay!
-        int dataStartIndex = 0;
-        driveMessageData[dataStartIndex + 0] = (byte)(distError >> 8);
-        driveMessageData[dataStartIndex + 1] = (byte)distError;
-        driveMessageData[dataStartIndex + 2] = (byte)(rotationErrorInt >> 8);
-        driveMessageData[dataStartIndex + 3] = (byte)(rotationErrorInt);
+			// Byte stuffing, yay!
+			int dataStartIndex = 0;
+			driveMessageData [dataStartIndex + 0] = (byte)(distError >> 8);
+			driveMessageData [dataStartIndex + 1] = (byte)(distError);
+			driveMessageData [dataStartIndex + 2] = (byte)(rotationErrorInt >> 8);
+			driveMessageData [dataStartIndex + 3] = (byte)(rotationErrorInt);
 
-        int encodededLength = COBS.cobs_encode(ref driveMessageData, DRIVE_MESSAGE_DATA_BYTES, ref COBSEncodedMessage);
-//        print("Encoded message (" + encodededLength + ")" + BitConverter.ToString(COBSEncodedMessage));
+			int encodededLength = COBS.cobs_encode (ref driveMessageData, DRIVE_MESSAGE_DATA_BYTES, ref COBSEncodedMessage);
+			//        print("Encoded message (" + encodededLength + ")" + BitConverter.ToString(COBSEncodedMessage));
 
-        byte[] finalMessage = new byte[encodededLength + 1];
-        Buffer.BlockCopy(COBSEncodedMessage,0, finalMessage,1, encodededLength);
-        finalMessage[0] = 0;
+			byte[] finalMessage = new byte[encodededLength + 1];
+			Buffer.BlockCopy (COBSEncodedMessage, 0, finalMessage, 1, encodededLength);
+			finalMessage [0] = 0;
 
-        //print("Initial message (6)" + BitConverter.ToString(driveMessageData) + "\nFinal message: " + BitConverter.ToString(finalMessage));
+			//print("Initial message (6)" + BitConverter.ToString(driveMessageData) + "\nFinal message: " + BitConverter.ToString(finalMessage));
 
-		if (arduinoSerialPort != null && arduinoSerialPort.IsOpen) {
-			arduinoSerialPort.Write (finalMessage, 0, encodededLength + 1);
+			if (arduinoSerialPort != null && arduinoSerialPort.IsOpen) {
+				arduinoSerialPort.Write (finalMessage, 0, encodededLength + 1);
+			}
+			//arduinoSerialPort.BaseStream.Flush();
 		}
-        //arduinoSerialPort.BaseStream.Flush();
     }
 
 
@@ -209,6 +220,9 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
 	}
 	public void OnTrackingLost(){
 		tracking = false;
+
+		// Stop as soon as we lose tracking
+		zeroDrive();
 	}
 	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus){
 		if (newStatus == TrackableBehaviour.Status.DETECTED ||
@@ -220,15 +234,15 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
 		}
 	}	
 
-
-
-
-
-
     void Start(){
         print("RoboCar Following Driver Starting...");
 
 		ARCamera = GameObject.Find("ARCamera");
+		if (ARCamera == null){
+			print("ERROR: Could not find camera object");
+		}
+
+		driveMessageData = new byte[DRIVE_MESSAGE_DATA_BYTES];  // setup output message 
 
 		// Register to receive OnTrackingFound and OnTrackingLost events
 		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
@@ -238,10 +252,10 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
 			
 
         // TODO: filter port names to find arduinos, then connect to each of those, monitor it's messages for the drive teensy id number, and only connect to that arduino
-        GetPortNames();
-        //OpenArduinoSerialPortConnection();
-        
-		//sendAck();
+		GetPortNames();
+        OpenArduinoSerialPortConnection();
+		sendAck();
+
         // Start reading from the arduino serial port
         //        StartCoroutine(
         //            AsynchronousReadFromArduino
@@ -251,9 +265,9 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
         //           )
         //       );
 
-		driveMessageData = new byte[DRIVE_MESSAGE_DATA_BYTES];  // setup output message
     }
 
+	// TODO: implement an actual emergency stop command
     void zeroDrive() {
         // reset our position
         distanceError_m = 0.0f;
@@ -263,14 +277,13 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
     }
 
     public string ReadFromArduino(int timeout = 0){
-		if (arduinoSerialPort != null) {
+		if (arduinoSerialPort != null && arduinoSerialPort.IsOpen) {
 			if (timeout > 0) {
 				arduinoSerialPort.ReadTimeout = timeout;
 			}
-
 			try {
 				//            if (arduinoSerialPort != null && arduinoSerialPort.IsOpen && arduinoSerialPort.BytesToRead > 0){ // If there are any bytes to read in the input buffer
-				return arduinoSerialPort.ReadLine ();
+				return arduinoSerialPort.ReadLine();
 				//            }else{
 				//                return null;
 				//            }
@@ -285,13 +298,14 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
 
         // Print anything received from the arduino.
         // TODO: setup async serial read
-//        string received = ReadFromArduino(1);
-//        if (received != null && received.Length > 7){
-//            print("RECEIVED: " + received);
-//            //if (received.Equals(DRIVE_ARDUINO_ADDRESS)){
-//            //    sendAck();
-//            //}
-//        }
+        string received = ReadFromArduino(1);
+		if (received != null){// && received.Length > 5){
+            print("RECEIVED: " + received);
+			if (received.Trim().Equals(DRIVE_ARDUINO_ADDRESS)){
+				print("ACK");
+                sendAck();
+            }
+        }
 
         // If we are tracking our marker in this frame, go towards it
         if (tracking) {
@@ -302,13 +316,10 @@ public class RobotFollowDriver : MonoBehaviour, ITrackableEventHandler
 			distanceError_m = Mathf.Sqrt((cameraToTarget.x*cameraToTarget.x) + (cameraToTarget.z*cameraToTarget.z)) - FOLLOWING_DISTANCE_M; // TODO: won't work if tracked object is behind us..bust still tracked..somehow
 			angleError_deg = Mathf.Atan2(cameraToTarget.x, cameraToTarget.z) * Mathf.Rad2Deg;
 
-			sendDriveCommand();
-
         // If we can't find the marker, stop
-        }else{
-            //print("tracking lost");
-            zeroDrive();
         }
-        
+
+		// Whether we're tracking or not, send a command eiter way so the arduino knows we still care about it
+		sendDriveCommand();
     }
 }
