6f777cb2a08f3e334538bb1d08d30cf162676121
Daniel Fitzgerald <djfitz@mit.edu>
2017-04-19 12:46:28 -0400

    Debug console to screen

--- RoboCar1/Assets/Scripts/FPSDisplay.cs
+++ RoboCar1/Assets/Scripts/FPSDisplay.cs
@@ -1,8 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO.Ports;
 
 public class FPSDisplay : MonoBehaviour
 {
+
+	//http://answers.unity3d.com/questions/152035/printtogui.html
+	static string myLog;
+	private string output = "";
+	private string stack = "";
+	void OnEnable () {
+		Application.RegisterLogCallback (HandleLog);
+	}
+	void OnDisable () {
+		// Remove callback when object goes out of scope
+		Application.RegisterLogCallback(null);
+	}
+	void HandleLog (string logString, string stackTrace, LogType type) {
+		output = logString;
+		stack = stackTrace;
+		myLog = output + "\n" + myLog;
+		if (myLog.Length > 5000){
+			myLog = myLog.Substring(0, 4000);
+		}
+	}
+
+
 	float deltaTime = 0.0f;
 
 	void Update()
@@ -12,13 +35,18 @@ public class FPSDisplay : MonoBehaviour
 
 	void OnGUI()
 	{
-		int w = Screen.width, h = Screen.height;
+		//http://answers.unity3d.com/questions/152035/printtogui.html
+		//if (!Application.isEditor){ //Do not display in editor ( or you can use the UNITY_EDITOR macro to also disable the rest
+			myLog = GUI.TextArea(new Rect(10, 10, Screen.width - 10, Screen.height - 10), myLog);
+		//}
 
+		int w = Screen.width, h = Screen.height;
+		const int sizePercent = 4;
 		GUIStyle style = new GUIStyle();
 
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+		Rect rect = new Rect(0, 0, w, h * sizePercent / 100);
 		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
+		style.fontSize = h * sizePercent / 100;
 		style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
